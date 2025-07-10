5517555012961a6f06ec6d840d15685f419b970b
Claudio <claudioarevalo891@gmail.com>
2025-07-10 03:36:59 -0500

    Mis cambios locales

---
+++ src/aplicacion/Producto.java
@@ -0,0 +1,58 @@
+
+package aplicacion;
+
+
+public class Producto {
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    private String codigo;
+    private String nombre;
+    private double precioUnitario;
+    private String descripcion;
+        
+   
+
+    
+
+    public void mostrarProducto(){
+        imprime los datos delproducto.
+    }
+    String getCodigo()
+    public double getPrecioUnitario(){
+        devuelven los valores.
+     }
+    
+    
+    
+}
--- src/aplicacion/factura.java
+++ src/aplicacion/factura.java
@@ -1,13 +1,128 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package aplicacion;
 
-/**
- *
- * @author augusto
- */
 public class factura {
+
+    public double[] getPreciosUnitarios() {
+        return preciosUnitarios;
+    }
+
+    public void setPreciosUnitarios(double[] preciosUnitarios) {
+        this.preciosUnitarios = preciosUnitarios;
+    }
+
+    public String getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(String numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public String getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public void setFechaEmision(String fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
+    public String getClienteNombre() {
+        return clienteNombre;
+    }
+
+    public void setClienteNombre(String clienteNombre) {
+        this.clienteNombre = clienteNombre;
+    }
+
+    public String getClienteDocumento() {
+        return clienteDocumento;
+    }
+
+    public void setClienteDocumento(String clienteDocumento) {
+        this.clienteDocumento = clienteDocumento;
+    }
+
+    public int getCantidadItems() {
+        return cantidadItems;
+    }
+
+    public void setCantidadItems(int cantidadItems) {
+        this.cantidadItems = cantidadItems;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public double getIgv() {
+        return igv;
+    }
+
+    public void setIgv(double igv) {
+        this.igv = igv;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public String[] getCodigosProductos() {
+        return codigosProductos;
+    }
+
+    public void setCodigosProductos(String[] codigosProductos) {
+        this.codigosProductos = codigosProductos;
+    }
+
+    public int[] getCantidades() {
+        return cantidades;
+    }
+
+    public void setCantidades(int[] cantidades) {
+        this.cantidades = cantidades;
+    }
+    
+    private String numeroFactura;
+    private String fechaEmision ;
+    private String clienteNombre;
+    private String clienteDocumento;
+    private int cantidadItems;
+    private double subtotal;
+    private double igv;
+    private double total;
+    private String[] codigosProductos;
+    private int[] cantidades;
+    private double[] preciosUnitarios;
+    
+
+    
+ public void agregarItem(String codigo, int cantidad, double precio){
+    
+        agrega un producto a la factura
+    }
+
+   public void calcularSubtotal(){
+        subtotal = (cantidad[i] * precio[i]);
+    }
+   public void calcularIGV(){
+       aplica IGV como 18% del subtotal
+    }   
+   public void calcularTotal(){
+       total = (subtotal + igv);
+     }
+   public void mostrarFactura(){
+           
+      muestra todos los datos de la factura(cliente, ítems, totales)
+    
+    }
     
 }
